907c8a56ea2117079af8b816b436d47e5bc1809f
saroj304 <you@example.com>
2023-05-28 15:02:15 +0545

    initial commit

---
+++ sanolagani/src/main/java/com/bitflip/sanolagani/SanolaganiApplication.java
@@ -0,0 +1,14 @@
+package com.bitflip.sanolagani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class SanolaganiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanolaganiApplication.class, args);
+	}
+
+}
---
+++ sanolagani/src/main/java/com/bitflip/sanolagani/models/User.java
@@ -0,0 +1,63 @@
+package com.bitflip.sanolagani.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	@Column(nullable = false)
+	private String fname;
+	@Column(nullable = false)
+	private String lname;
+	@Column(nullable = false)
+	private String email;
+	@Column(nullable = false)
+	private String password;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
