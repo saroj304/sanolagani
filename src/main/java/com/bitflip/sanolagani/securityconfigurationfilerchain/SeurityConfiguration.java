854759ede1c6c6622e2f5fc6177f7ab60c9b8c42
Ishwor_007 <raayaseetal@gmail.com>
2023-06-06 08:24:49 +0545

    managing frontend

--- src/main/java/com/bitflip/sanolagani/securityconfigurationfilerchain/SeurityConfiguration.java
+++ src/main/java/com/bitflip/sanolagani/securityconfigurationfilerchain/SeurityConfiguration.java
@@ -22,7 +22,7 @@ public class SeurityConfiguration extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 	 http
 	    .authorizeRequests()
-	    .antMatchers("/login","/register","/otpverify").permitAll()
+	    .antMatchers("/login","/register","/otpverify","/home","/").permitAll()
 	    .anyRequest()
 	    .authenticated()
 	    .and()
@@ -66,7 +66,7 @@ auth.authenticationProvider(daoAuthenticationProvider());
 @Override
 public void configure(WebSecurity web) throws Exception {
 	web.ignoring()
-	.antMatchers("/resources/**");
+	.antMatchers("/resources/**","/static/**","/css/**","/photos/**");
 }
 
 }
