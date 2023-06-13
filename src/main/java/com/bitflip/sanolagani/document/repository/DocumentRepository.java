b03e0a8029ed8bfcfdcc7651e4ecad80e7d57927
unknown <superstargajanikant@gmail.com>
2023-06-13 13:09:53 +0545

    document object repository

--- src/main/java/com/bitflip/sanolagani/document/repository/DocumentRepository.java
+++ src/main/java/com/bitflip/sanolagani/document/repository/DocumentRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.UUID;
 
 public interface DocumentRepository extends JpaRepository<Documents, UUID> {
-    @Query(value = "SELECT company_id FROM company WHERE email =:email ",nativeQuery = true)
+    @Query(value = "SELECT id FROM company WHERE email =:email ",nativeQuery = true)
     public int getCompanyId(String email);
 
 }
