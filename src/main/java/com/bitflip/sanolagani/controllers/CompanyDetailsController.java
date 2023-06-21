23f6689fb2d1714e81aa7c345d32899642f4e842
BESoft33 <superstargajanikant@gmail.com>
2023-06-21 08:00:56 +0545

    dynamic rendering of company details

--- src/main/java/com/bitflip/sanolagani/controllers/CompanyDetailsController.java
+++ src/main/java/com/bitflip/sanolagani/controllers/CompanyDetailsController.java
@@ -1,12 +1,25 @@
 package com.bitflip.sanolagani.controllers;
 
+import com.bitflip.sanolagani.models.Company;
+import com.bitflip.sanolagani.repository.CompanyRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class CompanyDetailsController {
-    @GetMapping("/company/info")
-    public String getCompany(){
+    @Autowired
+    CompanyRepo companyRepo;
+    @GetMapping("/company/{id}")
+    public String getCompany(@PathVariable("id") Integer id, Model model){
+        Company company = companyRepo.getReferenceById(id);
+        String name = company.getCompanyname();
+        String image = company.getImage();
+        model.addAttribute("company", company);
+//        model.addAttribute("image", image);
+//        model.addAttribute("id", id);
         return "company-info";
     }
     @GetMapping("/dashboard")
