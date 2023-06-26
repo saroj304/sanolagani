b4f1f8dfeb8b0fb27a3faa30c940ddf3e2472342
unknown <superstargajanikant@gmail.com>
2023-06-26 13:02:28 +0545

    Khalti payment service

---
+++ src/main/java/com/bitflip/sanolagani/service/PaymentService.java
@@ -0,0 +1,14 @@
+package com.bitflip.sanolagani.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import java.util.Map;
+
+public interface PaymentService {
+    public void savePaymentDetails();
+    public void getPaymentDetails();
+    public void deletePaymentDetails();
+    public void updatePaymentDetails();
+    public void verifyPayment(Map<String, String> paymentDetails) throws JsonProcessingException;
+}
+
