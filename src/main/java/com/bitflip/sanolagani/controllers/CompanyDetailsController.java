eaeb91184048be79eabc6a184fda033db1d24869
unknown <superstargajanikant@gmail.com>
2023-06-28 22:45:14 +0545

    added list all companies controller

--- src/main/java/com/bitflip/sanolagani/controllers/CompanyDetailsController.java
+++ src/main/java/com/bitflip/sanolagani/controllers/CompanyDetailsController.java
@@ -8,22 +8,30 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class CompanyDetailsController {
     @Autowired
     CompanyRepo companyRepo;
+    @GetMapping("/company")
+    public String getAllCompany(Model model){
+        List<Integer> companyId = companyRepo.getAllCompany();
+        List<Company> companies = new ArrayList<>();
+        for(int id: companyId)
+            companies.add(companyRepo.getReferenceById(id));
+
+        model.addAttribute("companies", companies);
+        return "company-list";
+    }
+
     @GetMapping("/company/{id}")
-    public String getCompany(@PathVariable("id") Integer id, Model model){
+    public String getCompany(@PathVariable("id") int id,Model model) {
         Company company = companyRepo.getReferenceById(id);
-        String name = company.getCompanyname();
-        String image = company.getImage();
-        model.addAttribute("company", company);
+        model.addAttribute("company",company);
         return "company-info";
     }
-<<<<<<< HEAD
-=======
-
->>>>>>> 1e2f6eb1ed7f74fd9af66cabc920f998271478d4
    
     @GetMapping("/company/details/{id}")
     public String getInvestCompanyDetails(@PathVariable("id") int id,Model model) {
--- src/main/java/com/bitflip/sanolagani/controllers/PaymentController.java
+++ src/main/java/com/bitflip/sanolagani/controllers/PaymentController.java
@@ -5,6 +5,7 @@ import com.bitflip.sanolagani.service.PaymentService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,12 +19,14 @@ public class PaymentController {
     PaymentService paymentService;
 
     @GetMapping("/verifypayment")
-    public String verifyPayment(@RequestParam("token") String token, @RequestParam("amount") String amount) throws JsonProcessingException {
+    public String verifyPayment(@RequestParam("token") String token, @RequestParam("amount") String amount, @RequestParam("cId") String id, Model model) throws JsonProcessingException {
         System.out.println(token + " " + amount);
         Map<String, String>  details = new HashMap<>();
         details.put("token", token);
         details.put("amount", amount);
-        paymentService.verifyPayment(details,token,amount);
-        return "redirect:/home";
+        boolean status = paymentService.verifyPayment(details,token,amount);
+        double amountRs = Double.parseDouble(amount)/100;
+        model = status ? model.addAttribute("message", "success") : model.addAttribute("message", "failed!");
+        return "redirect:/company/details/"+id +"/";
     }
 }
