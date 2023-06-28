6d76d1deb68b6f8a3e5bc52879d3aebc2a627ded
unknown <superstargajanikant@gmail.com>
2023-06-28 22:46:32 +0545

    added list all companies

--- src/main/java/com/bitflip/sanolagani/repository/CompanyRepo.java
+++ src/main/java/com/bitflip/sanolagani/repository/CompanyRepo.java
@@ -5,7 +5,12 @@ import org.springframework.data.jpa.repository.Query;
 
 import com.bitflip.sanolagani.models.Company;
 
+import java.util.List;
+
 public interface CompanyRepo extends JpaRepository<Company, Integer> {
 	@Query(value = "SELECT companyname FROM company WHERE id =:obj ",nativeQuery = true)
 	public String getCompanyName(Object obj);
+
+	@Query(value = "SELECT id FROM company",nativeQuery = true)
+	public List<Integer> getAllCompany();
 }
