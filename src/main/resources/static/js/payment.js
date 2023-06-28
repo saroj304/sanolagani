ce117c96fbb61d3b9a9daea496209c37bc87163d
unknown <superstargajanikant@gmail.com>
2023-06-28 22:48:30 +0545

    work on payment services and javascript

--- src/main/resources/static/js/payment.js
+++ src/main/resources/static/js/payment.js
@@ -1,33 +1,39 @@
-var config = {
-    // replace the publicKey with yours
-    "publicKey": "test_public_key_46b3052e00e145b5b2d988d17359b3a4",
-    "transactionID": "1234567890",
-    "productIdentity": "10",
-    "productName": "Company Name",
-    "productUrl": "http://localhost:8080/",
-    "paymentPreference": [
-        "KHALTI",
-        "EBANKING",
-        "MOBILE_BANKING",
-        "CONNECT_IPS",
-        "SCT",
-        ],
-    "eventHandler": {
-        onSuccess (payload) {
-            // hit merchant api for initiating verfication
+function khaltiConfig(){
+  var config = {
+      // replace the publicKey with yours
+      "publicKey": "test_public_key_46b3052e00e145b5b2d988d17359b3a4",
+      "transactionID": "1234567890",
+      "productIdentity": "10",
+      "productName": "Company Name",
+      "productUrl": "http://localhost:8080/",
+      "paymentPreference": [
+          "KHALTI",
+          "EBANKING",
+          "MOBILE_BANKING",
+          "CONNECT_IPS",
+          "SCT",
+          ],
+      "eventHandler": {
+          onSuccess (payload) {
+              // hit merchant api for initiating verfication
+              let companyId = document.getElementById("company-id").innerText;
+              console.log(companyId);
+              window.location.href = "/verifypayment?token="+payload.token+"&amount="+payload.amount+"&cId="+companyId;
+          },
+          onError (error) {
+              let errorMsg = document.getElementById("payment-error-message");
+//              errorMsg.innerHTML = error['token']
+          },
+          onClose () {
+              console.log('widget is closing');
+          }
+      }
+  };
 
-            window.location.href = "/verifypayment?token="+payload.token+"&amount="+payload.amount;
-//            verifyPayment(payload);
-        },
-        onError (error) {
-            console.log(error);
-        },
-        onClose () {
-            console.log('widget is closing');
-        }
-    }
-};
+  return config;
+}
 
+let config = khaltiConfig();
 var checkout = new KhaltiCheckout(config);
 var btn = document.getElementById("payment-button");
 btn.onclick = function () {
@@ -44,22 +50,6 @@ btn.onclick = function () {
        
 }
 
-function verifyPayment(payload) {
-    let verifyUrl = 'https://cors-anywhere.herokuapp.com/'+'https://khalti.com/api/v2/payment/verify/';
-    let token = payload.token;
-    let amount = payload.amount;
-    let data = {token: token, amount: amount};
-
-    axios.get( verifyUrl, 
-                data, 
-                {'Authorization':'Key test_secret_key_4e85bb4e70114300844afa1e59abfb00'})
-        .then(function(response) {
-            console.log(response);
-        })
-        .catch(function(error) {
-            console.log(error);
-        });
-}
 
 var fundManagement = document.getElementById("fund-management");
 var paymentOptions = document.getElementById("payment-options");
