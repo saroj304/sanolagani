d699ed3baa2409de5659f54c037d2b89224a28d5
Ishwor_007 <raayaseetal@gmail.com>
2023-06-28 20:02:03 +0545

    managing collateral once paid

--- src/main/java/com/bitflip/sanolagani/controllers/PaymentController.java
+++ src/main/java/com/bitflip/sanolagani/controllers/PaymentController.java
@@ -4,6 +4,8 @@ package com.bitflip.sanolagani.controllers;
 import com.bitflip.sanolagani.models.Collateral;
 import com.bitflip.sanolagani.models.Investment;
 import com.bitflip.sanolagani.models.Transaction;
+import com.bitflip.sanolagani.models.User;
+import com.bitflip.sanolagani.repository.CollateralRepo;
 import com.bitflip.sanolagani.service.PaymentService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,7 +23,11 @@ public class PaymentController {
 
     @Autowired
     PaymentService paymentService;
-
+    @Autowired 
+    CollateralRepo collaretal_repo;
+    @Autowired
+    UserController user_controller;
+    
     @GetMapping("/verifypayment")
     public String verifyPayment(@RequestParam("token") String token, 
     		                    @RequestParam("amount") String amount,
@@ -44,10 +50,21 @@ public class PaymentController {
     @PostMapping("/paywithcollateral")
     public String payWithCollateral(@RequestParam(value = "companyId") int id,@RequestParam("amount") double amount,
     							Transaction transaction,Investment investment,
-    							Collateral collateral) {
+    							Collateral collaterals) {
+        User user = user_controller.getCurrentUser();
+    	Collateral collateral = user.getCollateral();
+    	if(collateral.getCollateral_amount()>=amount) {
     	String amounts = String.valueOf(amount);
     	paymentService.saveTransactionDetails("collateral", amounts,id, transaction, 
-    											"paying with collateral", investment, collateral);
+    											"paying with collateral", investment, collaterals);
+    	
+    	double amt = collateral.getCollateral_amount();
+    	collateral.setCollateral_amount(amt-amount);
+    	collaretal_repo.save(collateral);
+    	return "redirect:/dashboard";
+    	}
+    	
+    	
     	return "redirect:/home";
     }
 }
