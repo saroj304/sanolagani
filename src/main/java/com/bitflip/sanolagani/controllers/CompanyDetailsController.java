1561eb106f8382011d7fea64f82d5c6338700aae
Ishwor_007 <raayaseetal@gmail.com>
2023-07-02 23:05:26 +0545

    redirecting company to change password while initial login

--- src/main/java/com/bitflip/sanolagani/controllers/CompanyDetailsController.java
+++ src/main/java/com/bitflip/sanolagani/controllers/CompanyDetailsController.java
@@ -5,15 +5,20 @@ import com.bitflip.sanolagani.models.Investment;
 import com.bitflip.sanolagani.models.User;
 import com.bitflip.sanolagani.repository.CompanyRepo;
 import com.bitflip.sanolagani.repository.InvestmentRepo;
+import com.bitflip.sanolagani.repository.UserRepo;
+
 import java.time.LocalDateTime;
 
 import java.util.List;
-
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CompanyDetailsController {
@@ -23,6 +28,42 @@ public class CompanyDetailsController {
     UserController usercontroller;
     @Autowired
     InvestmentRepo investrepo;
+    @Autowired
+    UserRepo userrepo;
+    
+    
+    @GetMapping("/change_password")
+    public String changePassword(Model model) {
+    	String email = (String) model.asMap().get("email");
+         model.addAttribute("email", email);
+    	return "changeinitialpassword";
+    }
+    @PostMapping("/change/initial/password")
+    public String getInitialPasswordChanged(@RequestParam("email") String email,
+    		                                @RequestParam("oldpassword") String oldpass,
+    		                                @RequestParam("password") String newpassword) {
+    	                         
+    	
+    	User user = userrepo.findByEmail(email);
+    	System.out.println(email);
+    	String hashpwd = user.getPassword();
+    	String  plainpwd = oldpass;
+    	BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    	boolean isMatch = passwordEncoder.matches(plainpwd, hashpwd);
+
+    	if (isMatch) {
+    	    user.setPassword(passwordEncoder.encode(newpassword));
+    	    user.getCompany().setPwd_change("true");
+    	    userrepo.save(user);
+    	    return "redirect:/home";
+    	} else {
+    	    System.out.println("Password does not match!");
+    	    return "redirect:/home";
+
+    	}
+    	
+    	
+    }
     
     @GetMapping("/company/{id}")
     public String getCompany(@PathVariable("id") Integer id, Model model){
@@ -59,15 +100,10 @@ public class CompanyDetailsController {
     @GetMapping("/company/details/{id}")
     public String getInvestCompanyDetails(@PathVariable("id") int id,Model model) {
     	Company company = companyRepo.getReferenceById(id);
-    	
     	model.addAttribute("company",company);
     	return "details";
     }
 
-   
-    @GetMapping("/khalti")
-    public String getKhaltiPage() {
-    	return "khaltiPayment";
-    }
+ 
 
 }
