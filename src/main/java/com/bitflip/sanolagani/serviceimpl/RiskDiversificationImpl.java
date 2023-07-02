ad728de221978235c418b32476056423bcccf021
Ishwor_007 <raayaseetal@gmail.com>
2023-07-02 23:09:30 +0545

    handling email services for otp or registration

--- src/main/java/com/bitflip/sanolagani/serviceimpl/RiskDiversificationImpl.java
+++ src/main/java/com/bitflip/sanolagani/serviceimpl/RiskDiversificationImpl.java
@@ -1,5 +1,6 @@
 package com.bitflip.sanolagani.serviceimpl;
 
+import com.bitflip.sanolagani.controllers.UserController;
 import com.bitflip.sanolagani.models.Company;
 import com.bitflip.sanolagani.models.Investment;
 import java.util.Optional;
@@ -20,6 +21,8 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.LinkedHashSet;
+
 import java.util.Map;
 
 @Service
@@ -30,44 +33,62 @@ public class RiskDiversificationImpl implements RiskDiversificationService{
 	UserRepo user_repo;
 	@Autowired 
 	CompanyRepo company_repo;
-	
+	@Autowired
+	UserController usercontroller;
 
 	 
 	public List<Company> recommendCompanies(List<Company> previousInvestments) {
+		User user = usercontroller.getCurrentUser();
         List<Company> recommendedCompanies = new ArrayList<>();
         Map<String, Integer> sectorFrequencyMap = new HashMap<>();
         for (Company company : previousInvestments) {
             String industrySector = company.getSector();
-            sectorFrequencyMap.put(industrySector, sectorFrequencyMap.getOrDefault(industrySector, 0) + 1);
-            
+           if(sectorFrequencyMap.containsKey(industrySector)) {
+        	   int existing_value = sectorFrequencyMap.get(industrySector);
+        	    sectorFrequencyMap.put(industrySector, existing_value+(int)company.getInvestments().stream()
+                                                                     .filter(investment -> investment.getUser().getId() == user.getId())
+                                                                     .filter(investment -> investment.getCompany().getSector().equals(industrySector))
+                                                                     .mapToDouble(Investment::getAmount)
+                                                                      .sum());
+           }else {
+            sectorFrequencyMap.put(industrySector,(int)company.getInvestments().stream()
+                                                    .filter(investment -> investment.getUser().getId() == user.getId())
+                                                    .filter(investment -> investment.getCompany().getSector().equals(industrySector))
+                                                    .mapToDouble(Investment::getAmount)
+                                                    .sum());
+           System.out.println(sectorFrequencyMap);
         }
+           }
+        
         String mostFrequentSector = "";
         int maxFrequency = 0;
         for (Map.Entry<String, Integer> entry : sectorFrequencyMap.entrySet()) {
             if (entry.getValue() > maxFrequency) {
                 mostFrequentSector = entry.getKey();
                 maxFrequency = entry.getValue();
+                System.out.println(mostFrequentSector);
             }
         }
+    	List<Company> companylist = company_repo.findAll();
+		for (Company company : companylist) {
+		    boolean isInvested = false;
+		    for (Company investedCompany : previousInvestments) {
+		        if (investedCompany.getSector().equals(company.getSector())) {
+		            isInvested = true;
+		            break;
+		        }
+		    }
+		    if (!isInvested) {
+		        recommendedCompanies.add(company);
+		    }
+		}	
         for (Company company : previousInvestments) {
             if (!company.getSector().equals(mostFrequentSector)) {
                 recommendedCompanies.add(company);
             }
         }
 
-			List<Company> companylist = company_repo.findAll();
-			for (Company company : companylist) {
-			    boolean isInvested = false;
-			    for (Company investedCompany : previousInvestments) {
-			        if (investedCompany.getId()==company.getId()) {
-			            isInvested = true;
-			            break;
-			        }
-			    }
-			    if (!isInvested) {
-			        recommendedCompanies.add(company);
-			    }
-			}		
+			
 		
        return recommendedCompanies;
    }
@@ -75,7 +96,7 @@ public class RiskDiversificationImpl implements RiskDiversificationService{
 	@Override
 	public List<Company> getPreviousInvestmentsForUser() {
 		List<Integer> company_ids = new ArrayList<>();
-		List<Company> company_list = new ArrayList<>();
+		LinkedHashSet<Company> company_list = new LinkedHashSet<>();
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		String username = authentication.getName();
 		User user= user_repo.findByEmail(username);
@@ -89,15 +110,15 @@ public class RiskDiversificationImpl implements RiskDiversificationService{
 		        
 		   for(int company_id:company_ids) {
 
-		   Company company = company_repo.getById(company_id);
+		   Company company = company_repo.getReferenceById(company_id);
 
 		   company_list.add(company);
 		    
 	   }
-	       
+	       List<Company> companies_list = new ArrayList<>(company_list);
 		   
-		
-		return company_list;
+		System.out.println("commpanylist: "+companies_list);
+		return companies_list;
 	}
 
 }
