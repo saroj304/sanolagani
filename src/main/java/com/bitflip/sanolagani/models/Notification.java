eae21fe656477365aee5ccdd7adf1e45d45d9481
Ishwor_007 <raayaseetal@gmail.com>
2023-07-03 22:18:11 +0545

    managing static and html files

---
+++ src/main/java/com/bitflip/sanolagani/models/Notification.java
@@ -0,0 +1,59 @@
+package com.bitflip.sanolagani.models;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name ="notification")
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+    @Column
+    private String message;
+    @Column
+    private LocalDateTime createdAt;
+    @Column
+    private int companyid;
+    @Column
+    private int userid;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+	public int getCompanyid() {
+		return companyid;
+	}
+	public void setCompanyid(int companyid) {
+		this.companyid = companyid;
+	}
+	public int getUserid() {
+		return userid;
+	}
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+    
+    
+    
+    
+}
