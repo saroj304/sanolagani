70887e511a062b50539c738b51a7672cd28d4468
unknown <superstargajanikant@gmail.com>
2023-07-06 11:25:43 +0545

    orm mapper for board_members table

---
+++ src/main/java/com/bitflip/sanolagani/repository/BoardMembersRepo.java
@@ -0,0 +1,7 @@
+package com.bitflip.sanolagani.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.bitflip.sanolagani.models.BoardMembers;
+
+public class BoardMembersRepo implements JpaRepository<BoardMembers, Integer>{
+    
+}
